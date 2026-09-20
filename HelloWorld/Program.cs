9507047822ea78994173040919ec4b8825ded6f4
youngest0daughter <yhy60249@gmail.com>
2026-09-20 17:24:19 +0300

    Step 8: arithmetic and calculator

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -18,3 +18,19 @@ Console.WriteLine($"Стипендия: {hasScholarship}");
 Console.Write("\nВведите ваш любимый предмет: ");
 string subject = Console.ReadLine() ?? "";
 Console.WriteLine($"Отлично! {firstName} любит {subject}.");
+
+Console.WriteLine("Калькулятор");
+Console.Write("Введите первое число: ");
+double num1 = double.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите второе число: ");
+double num2 = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"Сумма: {num1 + num2}");
+Console.WriteLine($"Разность: {num1 - num2}");
+Console.WriteLine($"Произведение: {num1 * num2}");
+
+if (num2 != 0)
+    Console.WriteLine($"Частное: {num1 / num2}");
+else
+    Console.WriteLine("Деление на ноль невозможно!");
